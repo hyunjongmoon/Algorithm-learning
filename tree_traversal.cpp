85ad0374bb68e8da7e9819979ec0030f00a77cb5
Hyunjong Moon <hyunjong.dev@gmail.com>
2020-08-12 09:08:49 +0900

    Add files via upload

---
+++ tree_traversal.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int number = 15;
+
+// 하나의 노드 정보를 선언합니다. 
+typedef struct node *treePointer;
+typedef struct node {
+	int data;
+	treePointer leftChild, rightChild;
+} node;
+
+// 중위 순회를 구현합니다.
+void inorder(treePointer ptr) {
+	if(ptr) {
+		inorder(ptr->leftChild);
+		cout << ptr->data << ' ';
+		inorder(ptr->rightChild);
+	}
+}
+
+// 후위 순회를 구현합니다.
+void postorder(treePointer ptr) {
+	if(ptr) {
+		postorder(ptr->leftChild);
+		postorder(ptr->rightChild);	
+		cout << ptr->data << ' ';
+	}
+}
+
+// 전위 순회를 구현합니다.
+void preorder(treePointer ptr) {
+	if(ptr) {
+		cout << ptr->data << ' ';
+		preorder(ptr->leftChild);
+		preorder(ptr->rightChild);
+	}
+}
+
+int main(void) {
+	node nodes[number + 1];
+	for(int i = 1; i <= number; i++) {
+		nodes[i].data = i;
+		nodes[i].leftChild = NULL;
+		nodes[i].rightChild = NULL;
+	}
+	for(int i = 1; i <= number; i++) {
+		if(i % 2 == 0) {
+			nodes[i / 2].leftChild = &nodes[i];
+		} 
+		else {
+			nodes[i / 2].rightChild = &nodes[i];
+		}
+	}
+	printf("전위 순회 : ");
+	preorder(&nodes[1]);
+	printf("\n");
+	
+	printf("중위 순회 : ");
+	inorder(&nodes[1]);
+	printf("\n");
+	
+	printf("후위 순회 : ");
+	postorder(&nodes[1]);
+	return 0;
+}
+ 
