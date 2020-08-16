6f988987cc80fc751d7e2f95d326afc5c1c379b7
Hyunjong Moon <hyunjong.dev@gmail.com>
2020-08-16 15:48:15 +0900

    Add files via upload

---
+++ Dp_Tiling2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int d[1001];
+
+int dp(int x) {
+	if(x == 1) return 1;
+	if(x == 2) return 3;
+	if(d[x] != 0) return d[x];
+	return d[x] = (dp(x - 1) + 2 * dp(x - 2)) % 10007;
+}
+ 
+int main(void) {
+	int x;
+	scanf("%d", &x);
+	printf("%d", dp(x));
+}
